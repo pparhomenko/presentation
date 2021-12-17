ac0de63c78330ce9c8391846d5c7153cd59c4049
pparhomenko <76627158+pparhomenko@users.noreply.github.com>
2021-12-18 01:32:08 +0300

    Add all files

---
+++ playground.js
@@ -0,0 +1,35 @@
+// const img = document.querySelector('.git img')
+
+// window.addEventListener('scroll',()=>{
+//     title.getBoundingClientRect()
+//     if(size.top<window.innerHeight/2){
+//         title.style.background =
+
+//     }
+    
+// })
+// const options = {
+//     threshold: 0.5,
+
+// }
+//  const observer = new IntersectionObserver(animate,options)
+// function animate(entries){
+// entries.forEach(entry=> {
+//     if(entry.isIntersecting){
+//         img.style.transform ='scale(2)'
+//     }
+// });
+// }
+
+//  observer.observe(img)
+// const controller = new ScrollMagic.Controller()
+
+// const scene = new ScrollMagic.Scene({
+//     triggerElement:'.git',
+//     triggerHook: 0.5,
+
+// })
+// .addIndicators()
+// .setClassToggle('.git') 
+// .addTo(controller);
+
